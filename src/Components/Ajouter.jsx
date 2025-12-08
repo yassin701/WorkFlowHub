d2109e87c0916b3cb167548b42f79e337ff2ee64
Yassine <yassinehamdoune55@gmail.com>
2025-12-08 11:59:35 +0100

    add space for work

---
+++ src/Components/Ajouter.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Ajouter() {
+  return (
+    <div>Ajouter</div>
+  )
+}
---
+++ src/Components/Modifier.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Modifier() {
+  return (
+    <div>Modifier</div>
+  )
+}
