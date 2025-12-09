2d5ed262439a0b48f6a5a55ce3c799ecdaa2d800
MAROUANE2345 <azertyquerty230230@gmail.com>
2025-12-09 12:53:07 +0100

    fix versions react

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import Corbeille from './pages/Corbeille'
 import Login from './pages/login'
 import './App.css'
 import { BrowserRouter ,Routes, Route } from 'react-router-dom'
-
+import { Toaster } from 'sonner';
+import Ajouter from './Components/Ajouter'
 
 function App() {
   return(
   <BrowserRouter>
+  <Toaster position="top-center" richColors />
    <Routes>
 
         <Route path="/" element={<Login/>} />
         <Route path="/Home" element={<Home />} />
         <Route path="/Corbeille" element={<Corbeille />} />
-             <Toaster position="top-center" richColors />
-               <Ajouter/>
+             
+              
    </Routes>
   </BrowserRouter>
+  // <Ajouter/>
   )
-
+}
 export default App
