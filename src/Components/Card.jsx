d44a85948b842be07e12b603936a34fbfba747df
Ayoub EL HERICHI <ayoub.elherichi@gmail.com>
2025-12-10 20:31:10 +0100

    Add <t.delete> condition to toDo... ect

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react';
-import './Card.css';
-import Modifier from './Modifier';
-import { Draggable } from '@hello-pangea/dnd';
+import { useState } from "react";
+import "./Card.css";
+import Modifier from "./Modifier";
+import { Draggable } from "@hello-pangea/dnd";
+
+//------------------------
+// 2. Impot Delete
+import Delete from "./Delete";
+//------------------------
 
 
 
 export default function Card({ task, index, onEdit }) {
   const [showEditPopUp, setShowEditPopUp] = useState(false);
 
+//------------------------
+  // 3. Add isDeleted state
+  const [isDeleted, setIsDeleted] = useState(false);
+//------------------------
+
   const handleClick = () => {
    if(onEdit) onEdit(task);
   };
+
+    if (isDeleted) return null;
 
   return (
     <Draggable draggableId={task.id.toString()} index={index}>
@@ -20,28 +32,30 @@ export default function Card({ task, index, onEdit }) {
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
-          
-            <>
-              <p 
-                  className={`Card ${
-                      task.priority === 'high'
-                        ? 'high'
-                        : task.priority === 'low'
-                        ? 'low'
-                        : 'medium'
-                      }
-                      ${task.status==='Done'?'DoneCard':''}`
-                  }
-                              onClick={handleClick}>
+          {showEditPopUp ? (
+            <Modifier />
+          ) : (
+            //---------------------------------------------------
+            // 4. Add Delete component and wrap Card in a div
+            <div className="card-with-delete">
+              <p
+                className={`Card ${
+                  task.priority === "high"
+                    ? "high"
+                    : task.priority === "low"
+                    ? "low"
+                    : "medium"
+                }`}
+                onClick={handleClick}
+              >
                 {task.title}
-
-               
               </p>
-            </>
-          
+              <Delete task={task} onDeleted={() => setIsDeleted(true)} />
+            </div>
+            //---------------------------------------------------
+          )}
         </div>
       )}
     </Draggable>
   );
 }
-
